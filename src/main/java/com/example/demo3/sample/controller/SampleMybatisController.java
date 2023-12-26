feacb266ee078d5649687fdae429419fc12af48c
lambdaret <lambdaret@gmail.com>
2023-12-26 22:35:26 +0900

    mybatis paging

--- src/main/java/com/example/demo3/sample/controller/SampleMybatisController.java
+++ src/main/java/com/example/demo3/sample/controller/SampleMybatisController.java
@@ -1,14 +1,19 @@
 package com.example.demo3.sample.controller;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.demo3.common.domain.ResVO;
 import com.example.demo3.sample.domain.BoardVO;
+import com.example.demo3.sample.domain.Paging;
 import com.example.demo3.sample.service.SampleMybatisService;
+import com.github.pagehelper.PageInfo;
 
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.AllArgsConstructor;
@@ -20,7 +25,7 @@ public class SampleMybatisController {
 	SampleMybatisService sampleMybatisService;
 	
 	@GetMapping("/sample/mybatis/selectBoardList")
-	public List<BoardVO> selectBoardList(HttpServletRequest req, BoardVO param) {
+	public List<BoardVO> selectBoardList(HttpServletRequest req, @RequestBody BoardVO param) {
 //		if(true)
 //			throw new MyException("0001", "error");
 //		if(true) {
@@ -29,6 +34,26 @@ public class SampleMybatisController {
 //		}
 		return sampleMybatisService.selectBoardList(param);
 	}
+
+	@GetMapping("/sample/mybatis/selectBoardPaging")
+	public ResVO selectBoardPaging(HttpServletRequest req, @RequestBody BoardVO param) {
+		PageInfo<BoardVO> pageInfo = new PageInfo<BoardVO>(sampleMybatisService.selectBoardPaging(param));
+		
+		List<BoardVO> list = pageInfo.getList();
+		
+		Paging paging = new Paging();
+		paging.setPageNo(pageInfo.getPageNum());
+		paging.setPageSize(pageInfo.getPageSize());
+		paging.setTotalCount(pageInfo.getTotal());
+		
+		Map<String, Object> data = new HashMap<>();
+		data.put("list", list);
+		data.put("paging", paging);
+		return new ResVO(data);
+
+	}
+	
+	
 	
 	@PostMapping("/sample/mybatis/insertBoard")
 	public void insertBoard(HttpServletRequest req, @RequestBody BoardVO param) {
--- src/main/java/com/example/demo3/sample/service/SampleMybatisService.java
+++ src/main/java/com/example/demo3/sample/service/SampleMybatisService.java
@@ -6,7 +6,10 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.example.demo3.sample.domain.BoardVO;
+import com.example.demo3.sample.domain.Paging;
 import com.example.demo3.sample.repository.BoardMapper;
+import com.github.pagehelper.Page;
+import com.github.pagehelper.PageHelper;
 
 import lombok.AllArgsConstructor;
 
@@ -17,6 +20,12 @@ public class SampleMybatisService {
 	
 	public List<BoardVO> selectBoardList(BoardVO vo) {
 		return boardMapper.selectBoardList(vo);
+	}
+	
+	public Page<BoardVO> selectBoardPaging(BoardVO vo) {
+		Paging paging = vo.getPaging();
+		PageHelper.startPage(paging.getPageNo(), paging.getPageSize());
+		return (Page<BoardVO>)boardMapper.selectBoardList(vo);
 	}
 	
 	@Transactional
